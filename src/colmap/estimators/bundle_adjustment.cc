25fc97605579720a3c3c5d90dec1106832145781
Johannes Schönberger <jsch@demuc.de>
2025-08-04 15:19:55 +0200

    Fix bundle adjustment performance regression due to changed Gauge (#3527)
    Based on reported issue https://github.com/colmap/colmap/issues/3479, I
    root-caused a performance regression in bundle adjustment to different
    gauge fixing behavior. In particular, the change from fixing the gauge
    with two cameras vs. fixing it with three points causes a ~40% slowdown
    in global bundle adjustment. No significant difference can be measured
    in local bundle adjustment. I could also not find a significant
    difference in the obtained solution quality. This PR reverts back to
    using two cameras for fixing the gauge. If no such two cameras can be
    found, which can occasionally happen in case of configured non-trivial
    rigs, we fall back to fixing three points.

--- src/colmap/estimators/bundle_adjustment.cc
+++ src/colmap/estimators/bundle_adjustment.cc
@@ -443,39 +443,23 @@ void FixGaugeWithThreePoints(
     Reconstruction& reconstruction,
     ceres::Problem& problem) {
   FixedGaugeWithThreePoints fixed_gauge;
-
-  // First check if we already fixed enough points in the problem.
   for (const auto& [point3D_id, num_observations] : point3D_num_observations) {
-    const Point3D& point3D = reconstruction.Point3D(point3D_id);
-    if (problem.IsParameterBlockConstant(point3D.xyz.data()) &&
-        fixed_gauge.MaybeAddFixedPoint(point3D.xyz) &&
-        fixed_gauge.num_fixed_points >= 3) {
-      return;
+    if (fixed_gauge.num_fixed_points >= 3) {
+      break;
     }
-  }
 
-  // Otherwise, fix sufficient points in the problem.
-  for (const auto& [point3D_id, num_observations] : point3D_num_observations) {
     Point3D& point3D = reconstruction.Point3D(point3D_id);
-    if (!problem.IsParameterBlockConstant(point3D.xyz.data()) &&
-        fixed_gauge.MaybeAddFixedPoint(point3D.xyz)) {
+    if (!problem.IsParameterBlockConstant(point3D.xyz.data())) {
       problem.SetParameterBlockConstant(point3D.xyz.data());
-      if (fixed_gauge.num_fixed_points >= 3) {
-        return;
-      }
     }
+
+    fixed_gauge.MaybeAddFixedPoint(point3D.xyz);
   }
 
-  LOG(WARNING)
-      << "Failed to fix Gauge due to insufficient number of fixed points: "
-      << fixed_gauge.num_fixed_points;
+  LOG_IF(WARNING, fixed_gauge.num_fixed_points < 3)
+      << "Failed to fix Gauge due to insufficient number of fixed points";
 }
 
-// Note that the following implementation does not handle all degenerate edge
-// cases well, e.g., where the selected two cameras are not well constrained
-// with respect to each other with shared observations. Furthermore, the
-// implementation could be more sophisticated for multi-camera rigs by selecting
-// camera pairs within a rig, etc.
 void FixGaugeWithTwoCamsFromWorld(
     const BundleAdjustmentOptions& options,
     const BundleAdjustmentConfig& config,
@@ -487,6 +471,25 @@ void FixGaugeWithTwoCamsFromWorld(
   if (!options.refine_rig_from_world) {
     return;
   }
+
+  // No need to fix the Gauge if two frames are already constant.
+  const size_t num_constant_frames = std::count_if(
+      image_ids.begin(),
+      image_ids.end(),
+      [&config, &reconstruction](image_t image_id) {
+        Image& image = reconstruction.Image(image_id);
+        return config.HasConstantRigFromWorldPose(image.FrameId());
+      });
+  if (num_constant_frames >= 2) {
+    return;
+  }
+
+  auto IsParameterizedRefSensor = [&problem](const Image& image) {
+    return image.FramePtr()->RigPtr()->IsRefSensor(
+               image.CameraPtr()->SensorId()) &&
+           problem.HasParameterBlock(
+               image.FramePtr()->RigFromWorld().translation.data());
+  };
 
   Image* image1 = nullptr;
   Image* image2 = nullptr;
@@ -535,6 +538,17 @@ void FixGaugeWithTwoCamsFromWorld(
     }
   }
 
+  // TODO(jsch): Notice that we could alternatively fall back to fixing the
+  // Gauge between two cameras in the same frame or in different frames. Since
+  // there are many different combinations to iterate through, we instead fall
+  // back to fixing the Gauge with three points for simplicity. Furthermore,
+  // once we support IMUs or other sensors, we should fix the Gauge differently.
+  if (image1 == nullptr || image2 == nullptr) {
+    LOG(WARNING) << "Failed to fix Gauge with two cameras. "
+                    "Falling back to fixing Gauge with three points.";
+    FixGaugeWithThreePoints(point3D_num_observations, reconstruction, problem);
+    return;
+  }
   // TODO(jsch): Notice that we could alternatively fall back to fixing the
   // Gauge between two cameras in the same frame or in different frames. Since
   // there are many different combinations to iterate through, we instead fall
@@ -859,8 +873,6 @@ class DefaultBundleAdjuster : public BundleAdjuster {
   void AddPointToProblem(const point3D_t point3D_id,
                          Reconstruction& reconstruction) {
     Point3D& point3D = reconstruction.Point3D(point3D_id);
-
-    size_t& num_observations = point3D_num_observations_[point3D_id];
 
     // Is 3D point already fully contained in the problem? I.e. its entire
     // track is contained in `variable_image_ids`, `constant_image_ids`,
