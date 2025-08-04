9f1232ca4e26c2c5f3e245afae6d80382b9505ff
Johannes Schönberger <jsch@demuc.de>
2025-08-04 15:19:55 +0200

    Fix bundle adjustment performance regression due to changed Gauge (#3527)
    Based on reported issue https://github.com/colmap/colmap/issues/3479, I
    root-caused a performance regression in bundle adjustment to different
    gauge fixing behavior. In particular, the change from fixing the gauge
    with two cameras vs. fixing it with three points causes a ~40% slowdown
    in global bundle adjustment. No significant difference can be measured
    in local bundle adjustment. I could also not find a significant
    difference in the obtained solution quality. This PR reverts back to
    using two cameras for fixing the gauge. If no such two cameras can be
    found, which can occasionally happen in case of configured non-trivial
    rigs, we fall back to fixing three points.

--- src/colmap/estimators/bundle_adjustment.cc
+++ src/colmap/estimators/bundle_adjustment.cc
@@ -520,12 +520,10 @@ void FixGaugeWithTwoCamsFromWorld(
   Eigen::Index frame2_from_world_fixed_dim = 0;
   for (const image_t image_id : image_ids) {
     Image& image = reconstruction.Image(image_id);
-    if (image1 == nullptr && 
-        image.FramePtr()->RigPtr()->IsRefSensor(image.CameraPtr()->SensorId())) {
+    if (image1 == nullptr && IsParameterizedRefSensor(image)) {
       image1 = &image;
-    } else if (image1 != nullptr && 
-               image1->FrameId() != image.FrameId() &&
-               image.FramePtr()->RigPtr()->IsRefSensor(image.CameraPtr()->SensorId())) {
+    } else if (image1 != nullptr && image1->FrameId() != image.FrameId() &&
+               IsParameterizedRefSensor(image)) {
       // Check if one of the baseline dimensions is large enough and
       // choose it as the fixed coordinate. If there is no such pair of
       // frames, then the scale is not constrained well.
